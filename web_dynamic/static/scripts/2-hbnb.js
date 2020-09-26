5469de887ee2e5b4b996e9b93772dc67bb768b29
nakadorx <nakadorx@gmail.com>
2020-09-26 17:50:45 +0200

    stl

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    const list = {};
+    $('INPUT[type="checkbox"]').click(function () {
+      if (this.checked) {
+        list[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete list[$(this).attr('data-id')];
+      }
+      text = Object.values(list).join(', ')
+      $('DIV.amenities h4').text(text)
+    });
+    // HBNB 2
+    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (obj) {
+    if (obj.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+        $('DIV#api_status').removeClass('available');
+    }
+    });
+  });
+  
